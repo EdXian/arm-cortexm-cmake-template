fa21615e3b3a1432da2e8c36a0fa24f7980b13a1
Edxian <f29993856@gmai.com>
2021-04-05 10:27:49 +0800

    update

---
+++ ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/DependInfo.cmake
@@ -0,0 +1,108 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/startup_stm32f746xx.s" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/startup_stm32f746xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F746xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F7xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F7xx_HAL_Driver/Inc"
+  "../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc"
+  "../Middlewares/ST/STM32_USB_Host_Library/Core/Inc"
+  "../Middlewares/Third_Party/FatFs/src"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1"
+  "../USB_HOST/App"
+  "../USB_HOST/Target"
+  "../FATFS/App"
+  "../FATFS/Target"
+  "../"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Core/Src/freertos.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Core/Src/freertos.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Core/Src/main.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Core/Src/main.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Core/Src/stm32f7xx_hal_msp.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Core/Src/stm32f7xx_hal_msp.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Core/Src/stm32f7xx_hal_timebase_tim.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Core/Src/stm32f7xx_hal_timebase_tim.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Core/Src/stm32f7xx_it.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Core/Src/stm32f7xx_it.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Core/Src/system_stm32f7xx.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Core/Src/system_stm32f7xx.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_cortex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_cortex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_exti.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_exti.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash_ex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash_ex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c_ex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c_ex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr_ex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr_ex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc_ex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc_ex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim_ex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim_ex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart_ex.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart_ex.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/list.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/port.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/port.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "/home/ed/arm-cortexm-cmake-template/ST-F746g/f746pro/build/CMakeFiles/STM32F74-GDISCO.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F746xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F7xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F7xx_HAL_Driver/Inc"
+  "../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc"
+  "../Middlewares/ST/STM32_USB_Host_Library/Core/Inc"
+  "../Middlewares/Third_Party/FatFs/src"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1"
+  "../USB_HOST/App"
+  "../USB_HOST/Target"
+  "../FATFS/App"
+  "../FATFS/Target"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
