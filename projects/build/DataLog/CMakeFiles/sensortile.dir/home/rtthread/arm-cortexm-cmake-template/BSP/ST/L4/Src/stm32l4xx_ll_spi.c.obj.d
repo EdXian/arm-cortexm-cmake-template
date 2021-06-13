22d1db98502760683f8d1e13b679ab22cddc35c9
EdXian <eddywu@nitring.com>
2021-06-13 09:14:12 -0700

    update sensortile project

---
+++ projects/build/DataLog/CMakeFiles/sensortile.dir/home/rtthread/arm-cortexm-cmake-template/BSP/ST/L4/Src/stm32l4xx_ll_spi.c.obj.d
@@ -0,0 +1,2 @@
+DataLog/CMakeFiles/sensortile.dir/home/rtthread/arm-cortexm-cmake-template/BSP/ST/L4/Src/stm32l4xx_ll_spi.c.obj: \
+ /home/rtthread/arm-cortexm-cmake-template/BSP/ST/L4/Src/stm32l4xx_ll_spi.c
---
+++ projects/build/DataLog/CMakeFiles/sensortile_flash.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for sensortile_flash.
+# This may be replaced when dependencies are built.
