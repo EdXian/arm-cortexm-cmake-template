aa19ce901f34cb5a7eb09bd5970497e7ba9a6e9a
EdXian <eddywu@nitring.com>
2021-06-18 05:15:28 -0700

    merger

--- cmake/uf2.cmake
+++ cmake/uf2.cmake
@@ -14,9 +14,9 @@ add_custom_command(
     TARGET ${project_name}
     #python ${CMAKE_SOURCE_DIR}/scripts/uf2/uf2conv.py -c -b 0x4000 -o ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${UF2_OUTPUT_FILE} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${BIN_OUTPUT_FILE}
 
-    COMMAND
+     COMMAND
      python ${BOOTLOADER_ROOT_PATH}/uf2-bootloader/lib/uf2/utils/uf2conv.py  -b 0x4000 -o "${project_name}.uf2" -c "${project_name}.bin"
-    DEPENDS ${project_name}.bin
+     DEPENDS ${project_name}.bin
      )
 #${BOOTLOADER_ROOT_PATH}/uf2-bootloader/lib/uf2/utils/uf2conv.py
 #${pjt_nm}.bin  -c -b ${app_start_address} -o ${CMAKE_CURRENT_BINARY_DIR}/${project_name}.uf2
